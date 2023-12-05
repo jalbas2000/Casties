d4c5ae952b1caf54b84be7eedfa760bdcfcc8f64
jalbas2000 <jalbas2000@yahoo.com>
2023-12-05 21:14:24 +0400

    End of Section 6

---
+++ src/Contracts/BidPlaced.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts;
+
+public class BidPlaced
+{
+    public string Id { get; set; }
+    public string AuctionId { get; set; }
+    public string Bidder { get; set; }
+    public DateTime BidTime { get; set; }
+    public int Amout { get; set; }
+    public string BidStatus { get; set; }
+
+}
